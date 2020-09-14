be7061df48c6b3a2bac96b0499133206789cf973
nawawan <mizutake0428@yahoo.co.jp>
2020-09-14 20:18:40 +0900

    遅延セグ木の二分探索以外の完成。 見本用のgoalを追加

--- library/segmant_tree/lazysegK.cpp
+++ library/segmant_tree/lazysegK.cpp
@@ -11,7 +11,7 @@ struct lazysegK{//非再帰
     vector<long long> dat, lazy;
     lazysegK(int n_){
         n = 1;
-        INF = 1e18;
+        INF = (1LL << 31) - 1;
         size = n_;
         h = ceil(log2(n_));
         while(n < n_) n *= 2;
@@ -23,17 +23,20 @@ struct lazysegK{//非再帰
         }
     }
 private:
+    long long ju(long long l, long long r){
+        return r != INF ? r : l;
+    }
     void push(int k){
         if(lazy[k] == INF) return;
         if(k < n){
-            lazy[2 * k] = lazy[k];
-            lazy[2 * k + 1] = lazy[k];
+            lazy[2 * k] = ju(lazy[k * 2], lazy[k]);
+            lazy[2 * k + 1] = ju(lazy[k * 2 + 1], lazy[k]);
         }
-        dat[k] = lazy[k];
+        dat[k] = ju(dat[k], lazy[k]);
         lazy[k] = INF;
     }
     void update(int k){//そのノードの更新
-        dat[k] = min(lazy[k * 2], lazy[k * 2 + 1]);
+        dat[k] = min(ju(dat[k * 2], lazy[k * 2]), ju(dat[k * 2 + 1], lazy[k * 2 + 1]));
     }
 public:
     void update(int l, int r, long long a){//l, r区間にaを作用
@@ -48,33 +51,83 @@ public:
         while(l < r){//区間にaを作用
             if(l & 1) lazy[l++] = a;
             if(r & 1) lazy[--r] = a;
+            l >>= 1;
+            r >>= 1;
         }
         while(templ >>= 1, tempr >>= 1, templ){//上側のdatの更新
             if(lazy[templ] == INF) update(templ);
             if(lazy[tempr] == INF) update(tempr);
         }
     }
-    long long query(int l, int r){
-        long long res = 0;
+    long long find(int l, int r){
+        long long res = INF;
         l += n;
         r += n - 1;
-        for(int i = h; h > 0; h--){//まず上からの伝搬
+        for(int i = h; i > 0; i--){//まず上からの伝搬
             push(l >> i);
             push(r >> i);
         }
+        r++;
         while(r > l){
             if(l & 1) {
-                push(l);
-                res = max(res, dat[l++]);
+                res = min(res, ju(dat[l], lazy[l]));
+                l++;
             }
             if(r & 1) {
                 r--;
-                push(r);
-                res = max(res, dat[r]);
+                res = min(res, ju(dat[r], lazy[r]));
             }
             l >>= 1;
             r >>= 1;
         }
         return res;
+    }
+};
+
+//再帰実装
+struct lazysegK{
+    int n;
+    vector<long long> dat, lazy;
+    long long MAX;
+    lazysegK(int n_){
+        n = 1;
+        MAX = 1e18;
+        while(n < n_) n *= 2;
+        dat.resize(2 * n);
+        lazy.resize(2 * n);
+        for(int i = 0; i < 2 * n; i++) dat[i] = MAX;
+    }
+    void eval(int k, int l, int r){
+        if(lazy[k] != MAX) dat[k] = min(dat[k], lazy[k]);
+        if(r - l > 1){
+            lazy[2 * k + 1] = min(lazy[2 * k + 1], lazy[k]);
+            lazy[2 * k + 2] = min(lazy[2 * k + 2], lazy[k]);
+        }
+        lazy[k] = MAX;
+    }
+    void update(int a, int b, long long x, int k = 0, int l = 0, int r = -1){
+        if(r < 0) r = n;
+        eval(k, l, r);
+        if(b <= l || r <= a) return;
+        if(a <= l && r <= b){
+            lazy[k] = x;
+            eval(k, l, r);
+        }
+        else{
+            update(a, b, x, 2 * k + 1, l, (l + r) / 2);
+            update(a, b, x, 2 * k + 2, (l + r) / 2, r);
+            dat[k] = min(dat[2 * k + 1], dat[2 * k + 2]);
+        }
+    }
+    long long find(int a, int b, int k = 0, int l = 0, int r = -1){
+        if(r < 0) r = n;
+        if(r <= a || b <= l) return MAX;
+        eval(k, l, r);
+        if(a <= l && r <= b) return dat[k];
+        else{
+            long long vl = find(a, b, k * 2, l, (l + r) / 2);
+            long long vr = find(a, b, k * 2 + 1, (l + r) / 2, r);
+            return min(vl, vr);
+        }
     }
 };
