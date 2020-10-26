c7285a025ec87acdeffb16fa3367d5189f072a61
nawawan <mizutake0428@yahoo.co.jp>
2020-10-26 23:39:33 +0900

    区間maxのBITを追加

--- library/segmant_tree/BIT.cpp
+++ library/segmant_tree/BIT.cpp
@@ -17,8 +17,41 @@ struct BIT{
         return res;
     }
     void add(int ind, long long t){
+        ind++;
         while(ind <= N){
             num[ind] += t;
+            ind += ind & -ind;
+        }
+    }
+};
+
+/*
+転倒数を求めるには
+座圧して順にadd(index, 1);
+sum(N) - sum(index + 1)を足す。
+*/
+
+//区間maxのBIT
+struct BIT{
+    vector<long long> num;
+    int N;
+    BIT(int n){
+        N = n;
+        num.resize(N + 1, 0);
+    }
+    long long bit_max(long long t){
+        long long res = 0;
+        while(t > 0){
+            res = max(res, num[t]);
+            t -= t & -t;
+        }
+        return res;
+    }
+    void add(int ind, long long t){
+        ind++;
+        int temp = ind;
+        while(ind <= N){
+            num[ind] = max(num[ind], t);
             ind += ind & -ind;
         }
     }
