84ca02d10524a5be41c9a3c3e44979a45b5ee6e0
nawawan <mizutake0428@yahoo.co.jp>
2021-01-06 01:23:57 +0900

    正式にkitamasa法を追加

--- library/sosuu.cpp
+++ library/sosuu.cpp
@@ -43,3 +43,24 @@ vector<int> div(int n){//高速素因数分解用の配列(n ~ 10^6ならOK)
     }
     return d;
 }
+//こっちが正式な高速素因数分解のコード(kitamasa法)
+vector<int> sieve(int n){
+    vector<int> res(n, 0);
+    iota(res.begin(), res.end(), 0);
+    for(int i = 2; i * i < n; i++){
+        if(res[i] < i) continue;
+        for(int j = i * i; j < n; j += i){
+            if(res[j] == j) res[j] = i;
+        }
+    }
+    return res;
+}
+//sieveで求まった配列を用いて素因数分解0(logn)
+vector<int> factor(int n, vector<int> &min_fac){
+    vector<int> t;
+    while(n > 1){
+        t.push_back(min_fac[n]);
+        n /= min_fac[n];
+    }
+    return t;
+}
