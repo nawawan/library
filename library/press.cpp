3d34e6b88e94232f5972c087c591a5baa88cd87d
nawawan <mizutake0428@yahoo.co.jp>
2021-01-30 00:09:31 +0900

    座圧のバグを修正

--- library/press.cpp
+++ library/press.cpp
@@ -8,7 +8,9 @@ template<typename T>
 vector<T> press(vector<T> &a){
     vector<T> temp = a;
     sort(temp.begin(), temp.end());
-    erase(unique(temp.begin(), temp.end()), temp.end());
-    a[i] = lower_bound(temp.begin(), temp.end(), a[i]) - temp.begin();
+    temp.erase(unique(temp.begin(), temp.end()), temp.end());
+    for(int i = 0; i < N; i++){
+        a[i] = lower_bound(temp.begin(), temp.end(), a[i]) - temp.begin();
+    }
     return temp;
 }
