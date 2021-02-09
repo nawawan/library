85b9b5ecd914586f3315d2b5f9702330cfaaf592
nawawan <mizutake0428@yahoo.co.jp>
2021-02-09 23:16:41 +0900

    行列ライブラリのひとまずの完成

--- library/matrix.cpp
+++ library/matrix.cpp
@@ -2,59 +2,28 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cassert>
 using namespace std;
 template<typename T> struct mat{
-    int N, M;//N x Mの行列(通常はN = M)
     vector<vector<T>> matrix;//行列
+    int N, M;
+    //const int MOD//必要なら
     mat() : N(0), M(0), matrix(0){}
     mat(int n, int m){
+        matrix.resize(n, vector<T>(m));
         N = n;
         M = m;
-        matrix.resize(N, vector<T>(M));
     }
     mat(vector<vector<T>> m){
-        N = m.size();
-        M = m[0].size();
         matrix = m;
-    }
-    mat(int n, int m, vector<vector<T>> m1){
-        N = n;
-        M = m;
-        matrix = m1;
-    }
-    void init(vector<vector<T>> m){
         N = m.size();
         M = m[0].size();
-        matrix = m; 
     }
-    mat mulmod(const mat &mat2, const T MOD){
-        mat res(N, M);
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < N; j++){
-                for(int k = 0; k < M; k++){
-                    res.matrix[i][j] += (*this).matrix[i][k] * mat2.matrix[k][j] % MOD;
-                    if(res.matrix[i][j] >= MOD) res -= MOD;
-                }
-            }
-        }
-        return res;
-    }
-    vector<T> operate(vector<T> vec){
+    vector<T> operate(vector<T> vec){//ベクトルにかける
         assert(vec.size() == M);
         vector<T> res(N);
         for(int i = 0; i < N; i++){
             for(int j = 0; j < M; j++) res[i] += matrix[i][j] * vec[j];
-        }
-        return res;
-    }
-    vector<T> operate(vector<T> vec, const T MOD){
-        assert(vec.size() == M);
-        vector<T> res(N);
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < M; j++){
-                res[i] += matrix[i][j] * vec[j] % MOD;
-                if(res[i] >= MOD) res[i] -= MOD;
-            }
         }
         return res;
     }
@@ -75,108 +44,77 @@ template<typename T> struct mat{
         }
         return vec;
     }
-    vector<T> repow(T K, vector<T> vec, const T MOD){//行列累乗(MODあり)
-        assert(N == M);
-        while(K > 0){
-            if(K & 1) vec = operate(vec, MOD);
-            vector<vector<T>> temp(N, vector<T>(M));
-            for(int i = 0; i < N; i++){
-                for(int j = 0; j < N; j++){
-                    for(int k = 0; k < N; k++){
-                        temp[i][j] += matrix[i][k] * matrix[k][j] % MOD;
-                        if(temp[i][j] >= MOD) temp[i][j] -= MOD;
-                    }
-                }
-            }
-            matrix.swap(temp);
-            K >>= 1;
-        }
-        return vec;
+    vector<T> operator[](const int i) const{
+        return matrix[i];
     }
-    vector<vector<T>> repow(T K, vector<vector<T>> m, const T MOD){//行列累乗(MODあり)単位行列とかとかける
-        assert(N == M && m.size() == N && m[0].size() == M);
-        mat m1(m);
-        while(K > 0){
-            if(K & 1) m1 = mulmod(m1, MOD);
-            vector<vector<T>> temp(N, vector<T>(M));
-            for(int i = 0; i < N; i++){
-                for(int j = 0; j < N; j++){
-                    for(int k = 0; k < N; k++){
-                        temp[i][j] += matrix[i][k] * matrix[k][j] % MOD;
-                        if(temp[i][j] >= MOD) temp[i][j] -= MOD;
-                    }
-                }
-            }
-            matrix.swap(temp);
-            K >>= 1;
-        }
-        return m1.matrix;
+    vector<T>& operator[](const int i){
+        return matrix[i];
     }
-    mat operator+(const vector<vector<T>> &mat2){
-        mat res(N, M);
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < M; j++) res.matrix[i][j] = (*this).matrix[i][j] + mat2[i][j]; 
-        }
-        return res;
+    mat& operator=(const mat& m){
+        return *m;
     }
-    mat operator+(const mat &mat1){
-        mat res(N, M);
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < M; j++) res.matrix[i][j] = (*this).matrix[i][j] + mat1.matrix[i][j]; 
-        }
-        return res;
-    }
-    mat operator-(const mat &mat1){
-        mat res(N, M);
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < M; j++) res.matrix[i][j] = (*this).matrix[i][j] - mat1.matrix[i][j]; 
-        }
-        return res;
-    }
-    mat operator-( const vector<vector<T>> &mat2){
-        mat res(N, M);
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < M; j++) res.matrix[i][j] = (*this).matrix[i][j] - mat2[i][j]; 
-        }
-        return res;
-    }
-    mat operator*(const mat &mat2){
+    mat& operator*=(const mat &mat2){
         mat res(N, M);
         for(int i = 0; i < N; i++){
             for(int j = 0; j < N; j++){
                 for(int k = 0; k < M; k++){
-                    res.matrix[i][j] += (*this).matrix[i][k] * mat2.matrix[k][j];
+                    res[i][j] += (*this)[i][k] * mat2[k][j];
                 }
             }
         }
-        return res;
+        return *this = res;
+    }
+    mat& operator^=(const mat &mat2){
+        assert(N == mat2.N && M == mat2.M);
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < M; j++) (*this)[i][j] ^= mat2[i][j];
+        }
+        return *this;
+    }
+    mat& operator|=(const mat &mat2){
+        assert(N == mat2.N && M == mat2.M);
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < M; j++) (*this)[i][j] |= mat2[i][j];
+        }
+        return *this;
+    }
+    mat& operator&=(const mat &mat2){
+        assert(N == mat2.N && M == mat2.M);
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < M; j++) (*this)[i][j] &= mat2[i][j];
+        }
+        return *this;
     }
     mat& operator+=(const mat &mat2){
         assert(N == mat2.N && M == mat2.M);
         for(int i = 0; i < N; i++){
-            for(int j = 0; j < M; j++) (*this).matrix[i][j] += mat2.matrix[i][j];
+            for(int j = 0; j < M; j++) (*this)[i][j] += mat2[i][j];
         }
         return *this;
     }
     mat& operator-=(const mat &mat2){
         assert(N == mat2.N && M == mat2.M);
         for(int i = 0; i < N; i++){
-            for(int j = 0; j < M; j++) (*this).matrix[i][j] -= mat2.matrix[i][j];
+            for(int j = 0; j < M; j++) (*this)[i][j] -= mat2[i][j];
         }
         return *this;
     }
-    mat& operator+=(const vector<vector<T>> &mat2){
-        assert(N == mat2.size() && M == mat2[0].size());
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < M; j++) (*this).matrix[i][j] += mat2[i][j];
-        }
-        return *this;
+    mat operator+(const mat &mat1){
+        return mat(*this) += mat1;
     }
-    mat& operator-=(const vector<vector<T>> &mat2){
-        assert(N == mat2.size() && M == mat2[0].size());
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < M; j++) (*this).matrix[i][j] -= mat2[i][j];
-        }
-        return *this;
+    mat operator-(const mat &mat1){
+        return mat(*this) -= mat1;
+    }
+    mat operator*(const mat &mat2){
+        return mat(*this) *= mat2;
+    }
+    mat operator&(const mat &mat1){
+        return mat(*this) &= mat1;
+    }
+    mat operator|(const mat &mat1){
+        return mat(*this) |= mat1;
+    }
+    mat operator^(const mat &mat1){
+        return mat(*this) ^= mat1;
     }
 };
