9632ec6aa63ba00b90f73691489d2fe0dd5091c1
nawawan <mizutake0428@yahoo.co.jp>
2021-02-16 21:37:09 +0900

    RollingHash完成

--- library/strings/RollingHash.cpp
+++ library/strings/RollingHash.cpp
@@ -5,68 +5,10 @@
 #include <random>
 #include <cassert>
 using namespace std;
-typedef unsigned long long ull;
-const ull base = 1e9 + 7;//除数は2^64と考える
-//オーバーフロー = 「MODをとること」となっている
-/*カス、衝突します
-https://www.slideshare.net/nagisaeto/rolling-hash-149990902
-を参照してください(2^64は普通にやばい)
-bool rolliha(string S, string T){//TがSに含まれているかどうか
-    int slen = S.size(), tlen = T.size();
-    if(slen < tlen) return false;
-    ull bt = 1;
-    for(int i = 0; i < tlen; i++) bt *= base;
-    ull sb = 0, tb = 0;//先頭tlen文字のハッシュ値
-    for(int i = 0; i < tlen; i++) sb = sb * base + (ull) S[i];
-    for(int i = 0; i < tlen; i++) tb = tb * base + (ull) T[i];
-    //Sの長さtlenの部分文字列がTに一致するか判定
-    for(int i = 0; i + tlen <= slen; i++){
-        if(sb == tb) return true;
-        if(i + tlen < slen) sb = sb * base - bt * (ull)S[i] + (ull)S[i + tlen];
-    }
-    return false;
-}
-
-
-int overlap(string S, string T){//Sの末尾、Tの先頭が何個一致するか
-    int slen = S.size(), tlen = T.size();
-    int ans = 0;
-    ull sb = 0, tb = 0, bt = 1;
-    for(int i = 0; i < min(slen, tlen); i++){
-        sb = sb + bt * (ull) S[slen - i - 1];
-        tb = tb + tb * (ull) T[i];
-        bt *= base;
-        if(sb == tb) ans++;
-    }
-    return ans;
-}
-
-//(使わないだろうけど、長さがk(<= S.size())で固定された文字列がSに含まれているかどうか判定する構造体)
-struct rolliha{
-    const ull base = 1e9 + 7;
-    map<ull, int> m;
-    ull bt;
-    rolliha(string S, int k){
-        int slen = S.size();
-        bt = 1;
-        for(int i = 0; i < k; i++) bt *= base;
-        ull sb = 0;
-        for(int i = 0; i < k; i++) sb = sb * base + (ull) S[i];
-        for(int i = 0; i + k <= slen; i++){
-            m[sb]++;
-            if(i + k < slen) sb = sb * base - bt * (ull)S[i] + (ull)S[i + k];
-        }
-    }
-
-    int judge(string T){//Tの長さは必ずk
-        int tlen = T.size();
-        ull tb = 0;
-        for(int i = 0; i < tlen; i++) tb = tb * base + (ull) T[i];
-        return m.count(tb);
-    }
-};
-*/
-//2^61 - 1を除数とするローリングハッシュの実装(安全)
+//2^61 - 1を除数とするローリングハッシュの実装(とても安全)
+//queryはO(1)
+//makehashはO(N)
+//LCPは(log(N))
 typedef unsigned long long ull;
 struct RollingHash{
     static const ull MOD = (1UL << 61) - 1;
@@ -132,8 +74,9 @@ struct RollingHash{
         expand(r - l);
         ull h = CalMod(POSITIVIZER + hash[r] - Mul(hash[l], power_mod[r - l]));
     }
-    int LCP(vector<ull>&a, int l1, vector<ull>&b, int l2){
-        int len = min((int)a.size() - l1, (int)b.size() - l2);
+    //hashがaのもののS[l1,...,r1]とbのもののS[l2,...,r2]が先頭からどれだけ一致しているかを返す
+    int LCP(vector<ull>&a, int l1, int r1, vector<ull>&b, int l2, int r2){
+        int len = min(r1 - l1, r2 - l2);
         int lb = 0, ub = len + 1;
         while(ub - lb > 1){
             int mid = (ub + lb) / 2;
