4827dcd8344870a8ded5b6f8edc350fde5d717b6
nawawan <mizutake0428@yahoo.co.jp>
2021-03-25 16:16:34 +0900

    garnerのアルゴリズムを追加

--- library/mod/mod.cpp
+++ library/mod/mod.cpp
@@ -17,3 +17,26 @@ long long mod_inverse(long long a, long long m){
     extgcd(a, m, x, y);
     return (m + x % m) % m;
 }
+
+//garnerのアルゴリズム
+//O(n^2)(nはmodの数)
+// x = r[i](mod m[i])を満たす最小の整数xを返す
+// rのサイズ == mのサイズは暗黙の了解
+// あるサイトではm[i]の累積積を持ってやっていた(これならO(m))けど、オーバーフローする可能性あるよね
+long long garner(vector<long long> &r, vector<long long> &m){
+    long long x = 0;
+    for(int i = 0; i < (int)r.size(); i++){
+        if(i == 0) x = r[i];
+        else{
+            x %= m[i];
+            long long t = r[i] - x;
+            for(int j = 0; j < i; j++){
+                t *= mod_inverse(m[j], m[i]);
+                t %= m[i];
+            }
+            if(t < 0) t += m[i];
+            x += t;
+            if(x >= m[i]) x -= m[i];
+        }
+    }
+}
