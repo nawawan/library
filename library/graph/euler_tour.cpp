0c17686c41c20fba6234247e46b3dbbd262b500b
nawawan <mizutake0428@yahoo.co.jp>
2021-03-26 17:57:39 +0900

    オイラーツアーを実装

---
+++ library/graph/euler_tour.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+using namespace std;
+struct Euler_Tour{
+    vector<int> order;
+    vector<int> begin, end;
+    Euler_Tour(vector<vector<int>> &G){
+        int root = 0;
+        int sz = G.size();
+        begin.resize(sz);
+        end.resize(sz);
+        int ind = 0;
+        dfs(root, ind, -1, G);
+    }
+    void dfs(int now, int &ind, int par, vector<vector<int>>&G){
+        order.push_back(now);
+        begin[now] = ind;
+        ++ind;
+        for(int e: G[now]){
+            if(e != par){
+                dfs(e, ind, now, G);
+                order.push_back(now);
+                ++ind;
+            }
+        }
+        end[now] = ind;
+    }
+};
