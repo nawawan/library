6ff3e05f08352915c9cf8578625caf36715029c0
nawawan <mizutake0428@yahoo.co.jp>
2021-09-16 07:54:34 +0900

    Mo'sアルゴリズムの雛形を完成させました

---
+++ library/Mo.cpp
@@ -0,0 +1,53 @@
+#include <vector>
+#include <cassert>
+using namespace std;
+//addとeraseを適宜変更(クエリが返す答えも適宜作る)
+struct Mo{
+    vector<int> lef, rig, ind;//クエリのindex
+    bool f = false;
+    int nl, nr, width, now = 0;//現在の左端、右端、区間の幅、現在見てるクエリ(now個目)0-indexed
+    Mo(int N, int Q) : ind(Q), nl(0), nr(0), width((int)sqrt(N)) {}
+    Mo(int N, int Q, vector<int>&L, vector<int>&R) : ind(Q), nl(0), nr(0), width((int)sqrt(N)), lef(L), rig(R) {}
+    void insert(int l, int r){
+        lef.push_back(l);
+        rig.push_back(r);
+    }
+    void insert(vector<int>&L, vector<int>&R){
+        lef = L;
+        rig = R;
+    }
+    void build(){
+        f = true;
+        iota(ind.begin(), ind.end(), 0);
+        sort(ind.begin(), ind.end(), [&](int x, int y){
+            if(lef[x] / width != lef[y] / width) return lef[x] < lef[y];
+            return bool((rig[x] < rig[y]) ^ (lef[x] / width % 2));
+        });
+    }
+    //開区間[lef[id], rig[id])に注意
+    //クエリのindexを返す
+    int increment(){
+        assert(f);
+        if(now == (int)ind.size()) return -1;
+        int id = ind[now];
+        while(nl > lef[id]) --nl, add(nl);
+        while(nl < lef[id]) erase(nl), ++nl;
+        while(nr > rig[id]) --nr, erase(nr);
+        while(nr < rig[id]) add(nr), ++nr;
+        ++now;
+        return id;
+    }
+    //区間に入る時
+    void add(){
+
+    }
+    //区間から外れる時
+    void erase(){
+
+    }
+    //現在見てるクエリについて答えを返す
+    //voidを適宜変更
+    void query(){
+        //return hoge;
+    }
+};
