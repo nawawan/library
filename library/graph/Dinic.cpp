f2466e5cdc33dca13d5550d2a06335003b5af3b8
nawawan <mizutake0428@yahoo.co.jp>
2022-08-21 19:59:03 +0900

    get_edge, change_edgeを追加

--- library/graph/Dinic.cpp
+++ library/graph/Dinic.cpp
@@ -5,20 +5,11 @@
 using namespace std;
 template<typename T>
 struct Dinic{
-    struct edge{
-        int to;
-        T cap;
-        int rev;
-        edge(int t, T c, int r) : to(t), cap(c), rev(r) {}
-    };
-    int V;
-    T INF;
-    vector<int> level, used;
-    vector<vector<edge>> G;
     Dinic(int N): INF(numeric_limits<T>::max()), V(N), used(N), level(N), G(N){}
     void add(int u, int v, T c){
         G[u].emplace_back(v, c, (int)G[v].size());
         G[v].emplace_back(u, 0, (int)G[u].size() - 1);
+        p.emplace_back(u, (int)G[u].size() - 1);
     }
     T solve(int s, int t){
         T flow = 0;
@@ -30,7 +21,29 @@ struct Dinic{
             while((f = dfs(s, t, INF)) > 0) flow += f;
         }
     }
+    edge get_edge(int i){
+        pair<int, int> pos = p[i];
+        edge e = G[p[i].first][p[i].second];
+        return e;
+    }
+    void change_edge(int i, T new_cap){
+        pair<int, int> pos = p[i];
+        edge &e = G[p[i].first][p[i].second];
+        e.cap = new_cap;
+        G[e.to][e.rev].cap = -new_cap;
+    }
 private:
+    struct edge{
+        int to;
+        T cap;
+        int rev;
+        edge(int t, T c, int r) : to(t), cap(c), rev(r) {}
+    };
+    int V;
+    T INF;
+    vector<int> level, used;
+    vector<vector<edge>> G;
+    vector<pair<int, int>> p; 
     void bfs(int s){
         level.assign(V, -1);
         queue<int> q;
